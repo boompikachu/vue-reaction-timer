f5d5b3c00ea792c759d148ea4cc50e6a29a33e09
Patthanat Thanintantrakun <boomeevee@icloud.com>
2021-01-11 11:28:06 +0700

    Done

---
+++ src/components/Result.vue
@@ -0,0 +1,49 @@
+<template>
+  <p>Reaction Time: {{ score }} ms</p>
+  <p class="rank">You got {{ rank }} rank</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        switch (true) {
+            case this.score < 150:
+                this.rank = "S";
+                break;
+            case this.score < 200:
+                this.rank = "A";
+                break;
+            case this.score < 250:
+                this.rank = "B";
+                break;
+            case this.score < 300:
+                this.rank = "C";
+                break;
+            case this.score < 350:
+                this.rank = "D";
+                break;
+            case this.score < 400:
+                this.rank = "F";
+                break;
+            default:
+                this.rank = "No"
+                break;
+        }
+    }
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+    
+</style>
